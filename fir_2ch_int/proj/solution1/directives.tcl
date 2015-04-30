3865b6ba97be7e8e5d7d8e6ab65add0767040f67
Alexander Rasborg Knudsen <ark@xli.dk>
2015-04-30 12:09:38 +0200

    Just added on github

---
+++ fir_2ch_int/proj/solution1/directives.tcl
@@ -0,0 +1,10 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 2014 Xilinx Inc. All rights reserved.
+############################################################
+set_directive_interface -mode ap_fifo "fir_top" din_i
+set_directive_interface -mode ap_fifo "fir_top" din_q
+set_directive_interface -mode ap_fifo "fir_top" dout_i
+set_directive_interface -mode ap_fifo "fir_top" dout_q
+set_directive_dataflow "fir_top"
